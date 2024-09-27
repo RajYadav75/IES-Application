43c288e49180cf40af22e4ad65cee62b6e71f18a
Raj Yadav <rajyadav751811@gmail.com>
2024-09-27 12:39:37 +0530

    Fetch Plan Method Inserted

--- ADMINAPI/src/main/java/in/raj/services/PlanService.java
+++ ADMINAPI/src/main/java/in/raj/services/PlanService.java
@@ -2,9 +2,11 @@ package in.raj.services;
 
 import in.raj.bindings.PlanForm;
 
+import java.util.List;
+
 public interface PlanService {
 
     public boolean createPlan(PlanForm planForm);
 
-
+    public List<PlanForm> fetchPlans();
 }
