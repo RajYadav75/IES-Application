37b15ad7b7890a04e45c10d5eb7da9d71a3864f6
Raj Yadav <rajyadav751811@gmail.com>
2024-09-27 12:30:10 +0530

    ViewAccount method inserted

--- ADMINAPI/src/main/java/in/raj/services/AccountService.java
+++ ADMINAPI/src/main/java/in/raj/services/AccountService.java
@@ -2,9 +2,11 @@ package in.raj.services;
 
 import in.raj.bindings.UserAccountForm;
 
+import java.util.List;
+
 public interface AccountService {
 
     public boolean createUserAccount(UserAccountForm accForm);
 
-
+    public List<UserAccountForm> fetchUserAccounts();
 }
