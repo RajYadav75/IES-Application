9cf5b020965c7c1ad49cc3b85d7d54ccc0b79671
Raj Yadav <rajyadav751811@gmail.com>
2024-09-27 12:43:56 +0530

    Creating Viewing Editing and ChangeStatus Plan Method Inserted

--- ADMINAPI/src/main/java/in/raj/services/PlanService.java
+++ ADMINAPI/src/main/java/in/raj/services/PlanService.java
@@ -9,4 +9,8 @@ public interface PlanService {
     public boolean createPlan(PlanForm planForm);
 
     public List<PlanForm> fetchPlans();
+
+    public PlanForm getPlanById(Integer planId);
+
+    public String changePlanStatus(Integer planId, String status);
 }
