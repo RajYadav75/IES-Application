4112d32148112632de31ed80c629e41a131e5d62
Raj Yadav <rajyadav751811@gmail.com>
2024-09-27 12:58:21 +0530

    Login RecoverPassword and Dashboard See Method Inserted

--- ADMINAPI/src/main/java/in/raj/services/UserService.java
+++ ADMINAPI/src/main/java/in/raj/services/UserService.java
@@ -1,4 +1,14 @@
 package in.raj.services;
 
+import in.raj.bindings.DashboardCard;
+import in.raj.bindings.LoginForm;
+
 public interface UserService {
+
+    public String login(LoginForm loginForm);
+
+    public boolean recoverPassword(String email);
+
+    public DashboardCard fetchDashboardInfo();
+
 }
