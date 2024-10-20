1123933d31185fdb2d0af655e3a1e8861cce4cc2
RajYadav75 <rajyadav751811@gmail.com>
2024-10-20 16:18:37 +0530

    Exception Handler Implemented

--- ADMINAPI/src/main/java/in/raj/exception/AppExceptionHandler.java
+++ ADMINAPI/src/main/java/in/raj/exception/AppExceptionHandler.java
@@ -1,11 +1,21 @@
 package in.raj.exception;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.time.LocalDateTime;
 
 @RestControllerAdvice
 public class AppExceptionHandler {
-    public ResponseEntity<> handleException(){
-        
+    @ExceptionHandler(value = Exception.class)
+    public ResponseEntity<AppException> handleException(String exMsg){
+        AppException ex = new AppException();
+        ex.setExCode("EX0003");
+        ex.setExDesc(exMsg);
+        ex.setExDate(LocalDateTime.now());
+
+        return new ResponseEntity<AppException>(ex, HttpStatus.INTERNAL_SERVER_ERROR);
     }
 }
